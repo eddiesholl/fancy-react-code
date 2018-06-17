ee3a09add7b0eb22349327c41c2d3c7d8d13c849
Eddie Sholl <esholl@zendesk.com>
2018-06-18 00:10:11 +1000

    Display root for components and non-components

--- src/core/tree/react-tree-item.ts
+++ src/core/tree/react-tree-item.ts
@@ -1,19 +1,21 @@
 import * as vscode from 'vscode';
 import { ICachedSourceFile } from 'fancy-react-core';
 const glob = require('glob');
+const path = require('path');
 
 export interface IReactTreeItem extends vscode.TreeItem {
   getChildren(): Promise<IReactTreeItem[]>;
   parent?: IReactTreeItem;
 }
 
-export type ReactTreeItem = ReactRootItem | ReactComponentItem;
+export type ReactTreeItem = ReactRootItem | ReactComponentItem | ReactBasicItem;
 
 type FileLoader = (filePath: string) => Promise<ICachedSourceFile>;
+// type SourceFilter = (sourceFile: ICachedSourceFile) => ReactTreeItem | undefined;
 
 export class ReactRootItem extends vscode.TreeItem implements IReactTreeItem {
   parent: undefined;
-  private components: Promise<ReactComponentItem[]> | undefined;
+  private childNodes: Promise<ReactTreeItem[]> | undefined;
   filePath: string;
   fileGlob: string;
   fileLoader: FileLoader;
@@ -27,37 +29,81 @@ export class ReactRootItem extends vscode.TreeItem implements IReactTreeItem {
   }
 
   getChildren(): Promise<ReactTreeItem[]> {
-    if (this.components === undefined) {
+    if (this.childNodes === undefined) {
       return new Promise((resolve, reject) => {
         glob(this.fileGlob, {}, (err: any, files: string[]) => {
           if (err) {
             reject(err);
           } else {
             const itemsForFiles = files.map((sourceFilePath) => {
-              return this.fileLoader(sourceFilePath).then((content) => {
-                if (content.component !== undefined) {
-                  return new ReactComponentItem(
-                    sourceFilePath,
-                    content.fileName,
-                    this,
-                  );
-                }
-              });
+              return this.fileLoader(sourceFilePath).then(this.sourceFilter);
             });
             Promise.all(itemsForFiles).then((components) => {
-              resolve(components.filter(item => item !== undefined) as ReactComponentItem[]);
+              resolve(components.filter(item => item !== undefined) as ReactTreeItem[]);
             });
           }
         });
       });
     } else {
-      return this.components;
+      return this.childNodes;
+    }
+  }
+
+  sourceFilter(content: ICachedSourceFile): ReactTreeItem | undefined {
+    return new ReactBasicItem(
+      content.filePath,
+      content.fileName,
+      this,
+    );
+  }
+}
+
+export class ReactComponentRootItem extends ReactRootItem {
+  sourceFilter(content: ICachedSourceFile): ReactTreeItem | undefined {
+    if (content.component !== undefined) {
+      return new ReactComponentItem(
+        content.filePath,
+        content.component.componentName,
+        this,
+      );
     }
   }
 }
 
+export class ReactBasicRootItem extends ReactRootItem {
+  sourceFilter(content: ICachedSourceFile): ReactTreeItem | undefined {
+    if (content.component === undefined) {
+      return new ReactBasicItem(
+        content.filePath,
+        content.fileName,
+        this,
+      );
+    }
+  }
+}
 
 export class ReactComponentItem extends vscode.TreeItem implements IReactTreeItem {
+  children: ReactTreeItem[];
+  parent: ReactTreeItem | undefined;
+  filePath: string;
+  iconPath: string;
+
+  constructor(filePath: string, label: string, parent?: ReactTreeItem, children: ReactTreeItem[] = []) {
+    super(label, vscode.TreeItemCollapsibleState.None);
+
+    this.filePath = filePath;
+    this.children = children;
+    this.parent = parent;
+
+    this.iconPath = path.join(process.cwd(), 'doc', 'F-R.png');
+  }
+
+  getChildren(): Promise<ReactTreeItem[]> {
+    return Promise.resolve([]);
+  }
+}
+
+export class ReactBasicItem extends vscode.TreeItem implements IReactTreeItem {
   children: ReactTreeItem[];
   parent: ReactTreeItem | undefined;
   filePath: string;
--- src/core/tree/react-tree-provider.ts
+++ src/core/tree/react-tree-provider.ts
@@ -2,11 +2,12 @@ import * as vscode from 'vscode';
 import { IState, SourceFileCache } from 'fancy-react-core';
 const path = require('path');
 
-import { ReactTreeItem, ReactRootItem } from './react-tree-item';
+import { ReactTreeItem, ReactRootItem, ReactComponentRootItem, ReactBasicRootItem } from './react-tree-item';
 import { TreeItem } from 'vscode';
 
 export class ReactTreeProvider implements vscode.TreeDataProvider<ReactTreeItem> {
-  rootItem: ReactRootItem;
+  componentRoot: ReactRootItem;
+  nonComponentRoot: ReactRootItem;
   cache: SourceFileCache;
 
   constructor({ project }: IState) {
@@ -20,11 +21,23 @@ export class ReactTreeProvider implements vscode.TreeDataProvider<ReactTreeItem>
 
     const srcGlob = srcFullPath + "/**/*.@(js|jsx|ts|tsx)";
 
-    this.rootItem = new ReactRootItem(srcFullPath, 'Components', this.cache.getFile.bind(this.cache), srcGlob);
+    this.componentRoot = new ReactComponentRootItem(
+      srcFullPath,
+      'Components',
+      this.cache.getFile.bind(this.cache),
+      srcGlob
+    );
+
+    this.nonComponentRoot = new ReactBasicRootItem(
+      srcFullPath,
+      'Non-Components',
+      this.cache.getFile.bind(this.cache),
+      srcGlob
+    );
   }
 
   getChildren(element: ReactTreeItem): Promise<ReactTreeItem[]> {
-    return element ? element.getChildren() : Promise.resolve([this.rootItem]);
+    return element ? element.getChildren() : Promise.resolve([this.componentRoot, this.nonComponentRoot]);
   }
 
   getParent(element: ReactTreeItem): vscode.ProviderResult<ReactTreeItem> {
